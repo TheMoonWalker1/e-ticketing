18b74b1ab987eeec9172b86d1554a7ddc8e3d9fe
Praneeth Bhandaru <praneethsbhandaru@gmail.com>
2023-04-16 00:57:20 -0400

    added fields to custom user

--- api/models.py
+++ api/models.py
@@ -2,8 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    username = models.CharField(max_length=12, blank=False, null=False, unique=True, primary_key=True)
+    sex = models.CharField(max_length=50, null=True)
     birthday = models.DateField(blank=True, null=True)
     nickname = models.CharField(max_length=50, blank=True)
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
+    # picture = models.
 
     class Meta:
         verbose_name_plural = "User"
